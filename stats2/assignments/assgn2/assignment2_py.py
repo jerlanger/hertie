9d48d5a4afc6579622f7863a3d3090d954e0ed6e
Joe Erlanger <erlangej+github@gmail.com>
2021-10-27 11:49:29 +0200

    finalized replication analysis

---
+++ stats2/assignments/assgn2/assignment2_py.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import researchpy as rp
+
+
+def t_test(col):
+    dfdf
+
+
+df = pd.read_csv("local/social_cohesion.csv", header=True)
+
+covariates = ["edu",
+              "church",
+              "income",
+              "status1",
+              "marital",
+              "isis_abuse",
+              "birth.year"]
