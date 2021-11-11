d9970d4a78ab257607c8242cfba8fde37840507c
Joe Erlanger <erlangej+github@gmail.com>
2021-11-11 01:04:43 +0100

    finalizing charts

--- policy_process/emissions_charts/emissions_charts.R
+++ policy_process/emissions_charts/emissions_charts.R
@@ -3,6 +3,7 @@ library(ggrepel)
 library(dplyr)
 library(cowplot)
 library(tidyverse)
+library(scales)
 
 #### 1. Load, Build Classifications, and Filter Data ####
 
@@ -78,14 +79,14 @@ df_r <- df %>%
        annex1,
        g77,
        country,
-       year, 
-       co2, 
+       year,
+       co2,
        co2_per_capita,
        co2_per_gdp,
        cumulative_co2,
-       trade_co2, 
-       share_global_co2, 
-       total_ghg, 
+       trade_co2,
+       share_global_co2,
+       total_ghg,
        ghg_per_capita) %>%
   mutate(n_dev_cats = ifelse(!oecd,1,0) + 
            ifelse(!hdi_cat == "Very High",1,0) +
@@ -143,6 +144,8 @@ ggplot2::ggplot(df_dist, aes(y=as.factor(n_dev_cats),
         axis.text = element_text(size = 20))
 
 #### 3. Category by Absolute CO2 by Year ####
+cbp1 <- c("#56B4E9", "#999999", "#E69F00", "#009E73",
+          "#0072B2", "#D55E00", "#F0E442", "#CC79A7")
 
 df_south <- df_r %>%
   pivot_longer(col=c("south_oecd","south_g77","south_hdi","south_wb","south_annex1")) %>%
@@ -151,45 +154,84 @@ df_south <- df_r %>%
   summarise(share_yr_co2 = sum(share_yr_co2, na.rm=T)) %>%
   ungroup()
 
-ggplot2::ggplot(df_south, aes(x=year, y=share_yr_co2, color=name)) +
+abs_line <- ggplot2::ggplot(df_south, aes(x=year, y=share_yr_co2, color=name)) +
   geom_line() +
   geom_point() +
-  scale_color_discrete(labels=c("Annex I","G77","HDI","OECD","WB")) +
+  scale_color_manual(labels=c("Annex I","G77","HDI","OECD","World Bank"), values=cbp1) +
+  scale_y_continuous(labels=scales::label_percent(accuracy=1), limits=c(.25,.75)) +
   labs(
     title = "Growth of Global South's Emissions",
     subtitle = "by \"Global South\" categories",
     x="",
-    y="Share of World CO2 Output"
+    y=expression("Pct Global "*CO[2]*" Output")
   ) +
+  geom_hline(yintercept=.5, color="red", linetype="longdash") +
   theme_minimal() +
-  theme(
-    aspect.ratio=
-  )
+  theme(panel.grid.minor = element_blank(),
+        axis.text = element_text(size=15),
+        legend.title = element_blank(),
+        legend.position = "top",
+        legend.direction = "horizontal",
+        legend.justification = "right",
+        legend.margin = margin(-20,30,0,0),
+        legend.text = element_text(size=10),
+        title = element_text(size=15))
+
+abs_line
 
 #### 4. Per Capita Distributions ####
 
 # Boxplot 
 capita_boxplot <- ggplot2::ggplot(df_newold, 
-                aes(x=paste(n_dev_cats,year), y=co2_per_capita)) +
-  geom_boxplot(aes(size=co2, color=as.factor(n_dev_cats)),outlier.color = "red") +
-labs(title = "Distribution of CO2 Tonnes Per Capita",
-     subtitle = "by \"Global South\" categories and year",
-     x = "",
-     y = "") +
+                                  aes(x=paste(n_dev_cats,year), 
+                                      y=co2_per_capita,
+                                      color=as.factor(n_dev_cats))) +
+  geom_boxplot(outlier.color = "red") +
+  geom_vline(xintercept=2.5, linetype="dashed") +
+  geom_vline(xintercept=4.5, linetype="dashed") +
+  geom_vline(xintercept=6.5, linetype="dashed") +
+  geom_vline(xintercept=8.5, linetype="dashed") +
+  geom_vline(xintercept=10.5, linetype="dashed") +
+  scale_color_manual(name="Number Categories", values=cbp1) +
+  scale_x_discrete(label=c("1990","2019",
+                           "1990","2019",
+                           "1990","2019",
+                           "1990","2019",
+                           "1990","2019",
+                           "1990","2019")) +
+  labs(title = "Distribution of CO2 Tonnes Per Capita",
+       subtitle = "by \"Global South\" categories and year",
+       x = "",
+       y = "") +
   theme_minimal() +
-  theme(legend.position = "none")
+  guides(color= guide_legend(nrow=1)) +
+  theme(legend.position = "top",
+        legend.direction = "horizontal",
+        legend.justification = "right",
+        legend.margin = margin(-25,40,0,0),
+        legend.text = element_text(size=12),
+        axis.text = element_text(hjust=0.5))
+
+capita_boxplot
 
 # Abs CO2
 abs_bar <- ggplot2::ggplot(df_newold, 
                 aes(x=paste(n_dev_cats,year), y=(co2/1000))) +
   geom_col(aes(fill=as.factor(n_dev_cats))) +
-  labs(title = "CO2 Production (Billion Tonnes)",
+  geom_vline(xintercept=2.5, linetype="dashed") +
+  geom_vline(xintercept=4.5, linetype="dashed") +
+  geom_vline(xintercept=6.5, linetype="dashed") +
+  geom_vline(xintercept=8.5, linetype="dashed") +
+  geom_vline(xintercept=10.5, linetype="dashed") +
+  scale_fill_manual(values=cbp1) +
+  labs(title = "CO2 Production (B)",
        x = "",
        y = "") +
   theme_minimal() +
   theme(legend.position = "none",
-        plot.title = element_text(size = 10, margin = margin(-5,0,-15,0)),
-        panel.grid.minor.y = element_blank())
+        plot.title = element_text(size = 10, margin = margin(-10,0,-20,0)),
+        panel.grid.minor.y = element_blank(),
+        axis.text.x = element_blank())
 
 plot_grid(capita_boxplot,abs_bar, ncol=1, rel_heights = c(3,1))
 
@@ -197,16 +239,17 @@ plot_grid(capita_boxplot,abs_bar, ncol=1, rel_heights = c(3,1))
 
 scatter_1990 <- ggplot2::ggplot(df_newold %>%
                                   filter(year==1990),
-                                aes(x=co2_per_capita, y=share_global_co2, color=as.factor(n_dev_cats))) +
+                                aes(x=co2_per_capita, y=share_yr_co2, color=as.factor(n_dev_cats))) +
   geom_point(aes(size=cumulative_co2, color=as.factor(n_dev_cats))) +
   scale_size_continuous(limits=c(0,413000)) +
+  scale_color_manual(values=cbp1) +
+  scale_y_continuous(labels=scales::label_percent(accuracy=1), limits=c(NA,.3)) +
   xlim(NA,42) +
-  ylim(NA,30) +
   geom_text_repel(aes(label=country)) +
   theme_minimal() +
   labs(title = "1990",
-       x = "CO2 Per Capita",
-       y = "Pct Global CO2",
+       x = expression(CO[2]*" Per Capita"),
+       y = expression("Pct Global "*CO[2]*" Output"),
        color = "# Development Categories",
        size = "Cumulative CO2") +
   theme(legend.position = "None",
@@ -214,21 +257,22 @@ scatter_1990 <- ggplot2::ggplot(df_newold %>%
 
 scatter_2019 <- ggplot2::ggplot(df_newold %>%
                                   filter(year==2019),
-                                aes(x=co2_per_capita, y=share_global_co2, color=as.factor(n_dev_cats))) +
+                                aes(x=co2_per_capita, y=share_yr_co2, color=as.factor(n_dev_cats))) +
   geom_point(aes(size=cumulative_co2, color=as.factor(n_dev_cats))) +
   scale_size_continuous(limits=c(0,413000)) +
+  scale_color_manual(values=cbp1) +
+  scale_y_continuous(labels=scales::label_percent(accuracy=1), limits=c(NA,.3)) +
   xlim(NA,42) +
-  ylim(NA,30) +
   geom_text_repel(aes(label=country)) +
   theme_minimal() +
   labs(title = "2019",
-       x = "CO2 Per Capita",
-       y = "Pct Global CO2",
+       x = expression(CO[2]*" Per Capita"),
+       y = expression("Pct Global "*CO[2]*" Output"),
        color = "# Development Categories",
        size = "Cumulative CO2") +
-  theme(
+  theme(legend.position = "None",
     plot.title = element_text(hjust = 0.5))
 
 plot_grid(scatter_1990,scatter_2019)
 
-#### 6. Misc ####
+#### Misc ####
